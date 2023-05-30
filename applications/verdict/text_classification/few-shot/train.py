96ecccc828258451eed978ac7dcfd7531ee34ff3
vic4code <vic4code@gmail.com>
2023-05-31 00:34:37 +0800

    tqdm display for eval still keeps error

--- applications/verdict/text_classification/few-shot/train.py
+++ applications/verdict/text_classification/few-shot/train.py
@@ -121,22 +121,7 @@ class PromptDataCollatorWithPadding(PromptDataCollatorWithPadding):
         return batch
 
 
-class CallbackHandler(CallbackHandler):
-    def on_prediction_step(self, args, state, control, num_chunks, eval_dataloader=None, **kwargs):
-        if state.is_local_process_zero and has_length(eval_dataloader.dataset):
-            if self.prediction_bar is None:
-                self.prediction_bar = tqdm(total=len(eval_dataloader), leave=self.training_bar is None)
-            self.prediction_bar.update(num_chunks)
-
-
 class PromptTrainer(PromptTrainer):
-    def __post_init__(self):
-        default_callbacks = DEFAULT_CALLBACKS + get_reporting_integration_callbacks(self.args.report_to)
-        callbacks = default_callbacks if callbacks is None else default_callbacks + callbacks
-        self.callback_handler = CallbackHandler(
-            callbacks, self.model, self.tokenizer, self.optimizer, self.lr_scheduler
-        )
-    
     def _get_train_sampler(self) -> Optional[paddle.io.Sampler]:
         if self.train_dataset is None or not has_length(self.train_dataset):
             return None
@@ -673,6 +658,7 @@ class PromptTrainer(PromptTrainer):
                 )
 
         self._total_loss_scalar += tr_loss.item()
+        self.state.global_step = 1
         train_loss = self._total_loss_scalar / self.state.global_step
 
         metrics = speed_metrics("train", start_time, num_samples=num_train_samples, num_steps=self.state.max_steps)
@@ -910,7 +896,7 @@ class PromptTrainer(PromptTrainer):
                     logits = self.preprocess_logits_for_metrics(logits, labels)
                 preds_host = logits if preds_host is None else nested_concat(preds_host, logits, padding_index=-100)
 
-            self.control = self.callback_handler.on_prediction_step(args, self.state, self.control, num_chunks=inputs['num_chunks'][-1].item())
+            self.control = self.callback_handler.on_prediction_step(args, self.state, self.control)
 
             # Gather all tensors and put them back on the CPU if we have done enough accumulation steps.
             if args.eval_accumulation_steps is not None and (step + 1) % args.eval_accumulation_steps == 0:
@@ -1098,6 +1084,7 @@ def main():
 
     # Deine the early-stopping callback.
     callbacks = [EarlyStoppingCallback(early_stopping_patience=4, early_stopping_threshold=0.0)]
+    # callbacks[0].on_prediction_step()
 
     # Initialize the trainer.
     trainer = PromptTrainer(
