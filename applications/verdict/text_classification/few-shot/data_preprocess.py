d730f073ca271aee568da313df2df1a851b509e3
vic4code <vic4code@gmail.com>
2023-06-06 14:39:01 +0800

    Add new data

--- applications/verdict/text_classification/few-shot/data_preprocess.py
+++ applications/verdict/text_classification/few-shot/data_preprocess.py
@@ -1,6 +1,8 @@
 import json
 import re
 import argparse
+import os
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--jsonl_file", default="doccano.jsonl", type=str, help="The doccano file exported from doccano platform.")
@@ -8,32 +10,44 @@ parser.add_argument("--save_dir", default="./data/jsonl/formal_data.jsonl", type
 args = parser.parse_args()
 
 def jsonl_to_model_inputs():
-    data_files = [
-                    # 'data/formal_dataset/文本標記/標記完成的匯出檔案-謹丞(NER)/文本分類0505.json',
-                    # 'data/formal_dataset/文本標記/標記完成的匯出檔案-謹丞(NER)/文本分類0508.json',
-                    'data/formal_dataset/文本標記/標記完成的匯出檔案-謹丞(NER)/文本分類0509.json'
-                ]
+    data_path = "data/formal_dataset/classification"
+    data_files = [filename for filename in Path(data_path).rglob('*.json')]
 
+    # data_files = [
+    #                 # 'data/formal_dataset/文本標記/標記完成的匯出檔案-謹丞(NER)/文本分類0505.json',
+    #                 # 'data/formal_dataset/文本標記/標記完成的匯出檔案-謹丞(NER)/文本分類0508.json',
+    #                 'data/formal_dataset/文本標記/標記完成的匯出檔案-謹丞(NER)/文本分類0509.json'
+    #             ]
+
+    print("Num files : ", len(data_files))
     whitespace = r"\s+"
 
-    ids = []
-    jids = []
+    id = set()
+    err_count = 0
+    preprocessed_data = []
 
     for data_file in data_files:
 
         with open(data_file, 'r') as f:
             raw_data = json.load(f)
         
-        preprocessed_data = []
-
         for data in raw_data:
-            preprocessed_data.append( 
-                {
-                    'id': data['id'],
-                    'data': re.sub(whitespace, "", data['data']['text']),
-                    'label': data['annotations'][0]['result'][0]['value']['choices']
-                }
-            )
+            try:
+                if data['annotations'][0]['result'][0]['id'] not in id:
+                    id.add(data['annotations'][0]['result'][0]['id'])
+                    preprocessed_data.append( 
+                        {
+                            'id': data['annotations'][0]['result'][0]['id'],
+                            'jid': data['data']['jid'],
+                            'data': re.sub(whitespace, "", data['data']['text']),
+                            'label': data['annotations'][0]['result'][0]['value']['choices']
+                        }
+                    )
+            except:
+                err_count += 1
+
+    print("Total number of data", len(preprocessed_data))
+    print("Error files: ", err_count)
 
     with open(args.save_dir, 'w', encoding="utf-8") as f:
         for item in preprocessed_data:
